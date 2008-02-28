4bce5a44982261e6857de3cca34c2d2fd00754e7
shingara <cyril.mougel@gmail.com>
2008-02-28 21:49:21 +0100

    * Add delete cache when you update settings

---
+++ app/sweepers/setting_sweeper.rb
@@ -0,0 +1,26 @@
+class SettingSweeper < ActionController::Caching::Sweeper
+  observe Setting
+
+  def after_create(setting)
+    expire_cache(setting)
+  end
+  
+  def after_update(setting)
+    expire_cache(setting)
+  end
+
+  def after_destroy(setting)
+    expire_cache(setting)
+  end
+
+
+private
+
+  def expire_cache(setting)
+    expire_page :controller => '/galleries', :action => 'index'
+    expire_page '/'
+    cache_dir = ActionController::Base.page_cache_directory
+    FileUtils.rm_r(Dir.glob(cache_dir+"/galleries/*")) rescue Errno::ENOENT
+    FileUtils.rm_r(Dir.glob(cache_dir+"/galleries")) rescue Errno::ENOENT
+  end
+end
