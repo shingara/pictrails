d2bf4c3b2e6f42fb6a568a48707da1ec15cf0000
shingara <cyril.mougel@gmail.com>
2008-02-29 20:40:22 +0100

    * Add permalink in URL to see all gallery

---
+++ db/migrate/006_add_permalink_to_galleries.rb
@@ -0,0 +1,14 @@
+class AddPermalinkToGalleries < ActiveRecord::Migration
+  def self.up
+    add_column :galleries, 'permalink', :string
+    add_index :galleries, 'permalink', :unique => true
+    Gallery.find(:all).each do |gallery|
+      gallery.define_permalink
+      gallery.save
+    end
+  end
+
+  def self.down
+    remove_column :galleries, 'permalink'
+  end
+end
