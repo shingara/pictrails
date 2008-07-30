1b6d043d4ea540c1213614d8abbf601dc254d208
shingara <cyril.mougel@gmail.com>
2008-07-30 15:08:24 +0200

    fix cache doesn't delete when update gallery

--- app/sweepers/gallery_sweeper.rb
+++ app/sweepers/gallery_sweeper.rb
@@ -19,6 +19,15 @@ private
   def expire_cache(gallery)
     expire_page :controller => '/galleries', :action => :show, :id => gallery
     expire_page :controller => '/galleries', :action => 'index'
+
+    delete_cache_pagination_galleries(gallery)
+
     expire_page '/'
+  end
+  
+  # Delete the cache about pagination in galleries view
+  def delete_cache_pagination_galleries(gallery)
+    FileUtils.rm_r(Dir.glob(@cache_dir+"/galleries/page/*")) rescue Errno::ENOENT
+    FileUtils.rm_r(Dir.glob(@cache_dir+"/galleries/#{gallery.permalink}/page/*")) rescue Errno::ENOENT
   end
 end
