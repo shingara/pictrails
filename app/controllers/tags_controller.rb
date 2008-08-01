c6e32f7a643cc32326c0fa0f3e35c5088a146553
shingara <cyril.mougel@gmail.com>
2008-08-01 22:06:24 +0200

    add view of tag view in tag sidebar

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,6 +4,7 @@ class TagsController < ApplicationController
   
   def show
     @tag = Tag.find_by_name params[:id]
+    @tags = [@tag]
     @taggings = @tag.taggings.paginate :page => params[:page],
       :per_page => this_webapp.pictures_pagination
   end
