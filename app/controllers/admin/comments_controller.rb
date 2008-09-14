2d579c023556e72a739b5acac0b56ab41eeb50f5
shingara <cyril.mougel@gmail.com>
2008-09-14 19:42:38 +0200

    add the destroy of comments

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -12,4 +12,16 @@ class Admin::CommentsController < Admin::BaseController
     @comment = Comment.find params[:id]
   end
 
+  def destroy
+    unless request.delete?
+      flash[:notice] = "you can't destroy a comment by get request"
+      redirect_to admin_comments_url
+      return
+    end
+    comment = Comment.find(params[:id])
+    comment.destroy
+    flash[:notice] = "Comment #{comment.id} is destroy"
+    redirect_to admin_comments_url
+  end
+
 end
--- spec/controllers/admin/comments_controller_spec.rb
+++ spec/controllers/admin/comments_controller_spec.rb
@@ -8,7 +8,8 @@ describe Admin::CommentsController do
     include AuthenticatedTestHelper
 
     before :each do
-     login_as :quentin 
+      login_as :quentin 
+      @comment = mock_model(Comment, :id => rand(30000))
     end
 
     describe 'action index' do
@@ -22,9 +23,6 @@ describe Admin::CommentsController do
     end
 
     describe 'action show' do
-      before :each do
-        @comment = mock_model(Comment, :id => rand(30000))
-      end
 
       it 'should see the comment with good param' do
         Comment.should_receive(:find).with(@comment.id.to_s).and_return(@comment)
@@ -37,6 +35,31 @@ describe Admin::CommentsController do
         Comment.should_receive(:find).with(@comment.id.to_s).and_raise(ActiveRecord::RecordNotFound)
         lambda {
           get 'show', :id => @comment.id
+        }.should raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+
+    describe 'action destroy' do
+      
+      it 'should not destroy if not post action' do
+        Comment.should_not_receive(:find).with(@comment.id.to_s).and_return(@comment)
+        get 'destroy', :id => @comment.id
+        response.should redirect_to(admin_comments_url)
+        flash[:notice].should == "you can't destroy a comment by get request"
+      end
+
+      it 'should destroy comment if good param' do
+        Comment.should_receive(:find).with(@comment.id.to_s).and_return(@comment)
+        @comment.should_receive(:destroy).and_return(true)
+        delete 'destroy', :id => @comment.id
+        response.should redirect_to(admin_comments_url)
+        flash[:notice].should == "Comment #{@comment.id} is destroy"
+      end
+      
+      it 'should render 404 if no id exist' do
+        Comment.should_receive(:find).with(4000000.to_s).and_raise(ActiveRecord::RecordNotFound)
+        lambda {
+          delete 'destroy', :id => 4000000
         }.should raise_error(ActiveRecord::RecordNotFound)
       end
     end
